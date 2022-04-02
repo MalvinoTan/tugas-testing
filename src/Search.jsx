44a11e0138a6224b12cc1a090115d0ab5d6d7767
Malvino Austin Tanura <malvinoaustintanura@gmail.com>
2022-04-02 12:41:41 +0700

    Mengupdate tugas Testing

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,8 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
- 
+
 const URL = 'http://hn.algolia.com/api/v1/search';
- 
+
 export default function Search() {
   const [stories, setStories] = useState([]);
   const [error, setError] = useState(null);
@@ -10,7 +10,7 @@ export default function Search() {
 
   async function handleFetch(event) {
     let result;
- 
+
     try {
       result = await axios.get(`${URL}?query=${value}`);
       setStories(result.data.hits);
@@ -19,15 +19,15 @@ export default function Search() {
       setError(error);
     }
   }
- 
+
   return (
     <div>
       <h2>Cari Cerita</h2>
-      <input onChange={e=> setValue(e.target.value)} type="text" placeholder="Tulis Cerita" />
+      <input onChange={e => setValue(e.target.value)} type="text" placeholder="Tulis Cerita" />
       <button type="button" onClick={handleFetch}>
         Cari Cerita
       </button> <br />
- 
+
       {error && <span>Ada yang error ...</span>}
 
       {stories.length > 0 && "Daftar Cerita"}
--- src/useInputValue.js
+++ src/useInputValue.js
@@ -2,9 +2,9 @@ import { useState } from "react";
 
 export const useInputValue = initialValue => {
   const [value, setValue] = useState(initialValue);
-   return {
-     value,
-     onChange: e => { 
+  return {
+    value,
+    onChange: e => {
       setValue(e.target.value || e.target.innerText);
     }
   };
